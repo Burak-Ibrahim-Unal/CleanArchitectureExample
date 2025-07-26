db6b212179151c716f83bd6c50160b23e570f662
Burak Ibrahim Unal <32199163+Burak-Ibrahim-Unal@users.noreply.github.com>
2025-07-26 23:02:09 +0300

    BookingErrors  and Booking

---
+++ BookingComExample/src/BookingComExample.Domain/Bookings/BookingErrors.cs
@@ -0,0 +1,16 @@
+﻿using BookingComExample.Domain.Abstractions;
+
+namespace BookingComExample.Domain.Bookings;
+
+public class BookingErrors
+{
+    public static Error NotFound = new("Booking.NotFound", "Booking with specified identifier is not found");
+    
+    public static Error Overlap = new("Booking.Overlap", "Booking overlaps with other booking");
+    
+    public static Error NotReserved = new("Booking.NotReserved", "Booking is not reserved");
+    
+    public static Error NotConfirmed = new("Booking.NotConfirmed", "Booking is not confirmed");
+    
+    public static Error AlreadyStarted = new("Booking.AlreadyStarted", "Booking is already started");
+}
