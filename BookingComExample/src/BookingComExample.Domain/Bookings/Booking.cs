549ab14a8ed3bc5e83e97ac6e6905d349d505968
Burak Ibrahim Unal <32199163+Burak-Ibrahim-Unal@users.noreply.github.com>
2025-07-26 22:31:50 +0300

    need BookingErrors but others codes are ready

--- BookingComExample/src/BookingComExample.Domain/Bookings/Booking.cs
+++ BookingComExample/src/BookingComExample.Domain/Bookings/Booking.cs
@@ -15,7 +15,7 @@ public sealed class Booking : Entity
         Money cleaningFee,
         Money totalPrice,
         BookingStatus status,
-        DateTime createdOnUtc) : base(id)
+        DateTime confirmedOnUtc) : base(id)
     {
     }
 
@@ -27,7 +27,7 @@ public sealed class Booking : Entity
     public Money AmenitiesUpCharge { get; private set; }
     public Money TotalPrice { get; private set; }
     public BookingStatus Status { get; private set; }
-    public DateTime CreatedOnUtc { get; private set; }
+    public DateTime ConfirmedOnUtc { get; private set; }
 
     public static Booking Reserve(
         Apartment apartment,
@@ -54,5 +54,72 @@ public sealed class Booking : Entity
         apartment.LastBookedOnUtc = createdOnUtc;
 
         return booking;
+    }
+
+    public Result Confirm(DateTime utcNow)
+    {
+        if (Status != BookingStatus.Reserved)
+        {
+            return Result.Failure(BookingErrors.NotPending);
+        }
+
+        Status = BookingStatus.Confirmed;
+        ConfirmedOnUtc = utcNow;
+
+        RaiseDomainEvent(new BookingReservationDomainEvent(Id));
+
+        return Result.Success();
+    }
+
+    public Result Reject(DateTime utcNow)
+    {
+        if (Status != BookingStatus.Reserved)
+        {
+            return Result.Failure(BookingErrors.NotPending);
+        }
+
+        Status = BookingStatus.Rejected;
+        ConfirmedOnUtc = utcNow;
+        
+        RaiseDomainEvent(new BookingReservationDomainEvent(Id));
+
+        return Result.Success();
+    }
+    
+    public Result Complete(DateTime utcNow)
+    {
+        if (Status != BookingStatus.Reserved)
+        {
+            return Result.Failure(BookingErrors.NotConfirmed);
+        }
+
+        Status = BookingStatus.Completed;
+        ConfirmedOnUtc = utcNow;
+        
+        RaiseDomainEvent(new BookingReservationDomainEvent(Id));
+
+        return Result.Success();
+    }
+    
+    public Result Cancel(DateTime utcNow)
+    {
+        if (Status != BookingStatus.Reserved)
+        {
+            return Result.Failure(BookingErrors.NotConfirmed);
+        }
+
+        var currentDate = DateOnly.FromDateTime(utcNow);
+
+        if (currentDate > Duration.Start)
+        {
+            return Result.Failure(BookingErrors.AlreadyStarted);
+        }
+
+        Status = BookingStatus.Cancelled;
+        ConfirmedOnUtc = utcNow;
+        
+        RaiseDomainEvent(new BookingReservationDomainEvent(Id));
+
+        return Result.Success();
     }
 }
